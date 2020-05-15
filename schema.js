f02102c04eef8a275e49f475105190dea2872400
Steven Polido <steven.polido@vistage.com>
2020-05-14 17:48:30 -0700

    Added more types to schema

--- schema.js
+++ schema.js
@@ -6,7 +6,8 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = require('graphql');
 
 // Profile Type 
@@ -20,6 +21,67 @@ const ProfileType = new GraphQLObjectType({
     }),
 });
 
+// Collection Type
+const CollectionType = new GraphQLObjectType({
+    name: 'Collection',
+    fields: () => ({
+        pagination: { type: PaginationType },
+        releases: { type: new GraphQLList(ReleasesType) }
+    })
+});
+
+// Pagination Type 
+const PaginationType = new GraphQLObjectType({
+    name: 'Pagination',
+    fields: () => ({
+        per_page: { type: GraphQLInt },
+        items: { type: GraphQLInt },
+        page: { type: GraphQLInt },
+        urls: { type: UrlType },
+        pages: { type: GraphQLInt }
+    }),
+});
+
+// Url Type
+const UrlType = new GraphQLObjectType({
+    name: 'Urls',
+    fields: () => ({
+        last: { type: GraphQLString },
+        next: { type: GraphQLString }
+    })
+});
+
+// Releases Type 
+const ReleasesType = new GraphQLObjectType({
+    name: 'Releases',
+    fields: () => ({
+        date_added: { type: GraphQLString },
+        basic_information: { type: BasicInformationType }
+    })
+});
+
+// BasicInformationType
+const BasicInformationType = new GraphQLObjectType({
+    name: 'BasicInformation',
+    fields: () => ({
+        id: { type: GraphQLInt },
+        year: { type: GraphQLInt },
+        title: { type: GraphQLString },
+        artists: { type: new GraphQLList(ArtistsType) },
+        cover_image: { type: GraphQLString }
+    })
+})
+
+// ArtistsType
+const ArtistsType = new GraphQLObjectType({
+    name: 'Artists',
+    fields: () => ({
+        name: { type: GraphQLString }
+    })
+})
+
+// Wantlist Type
+
 // Root Query 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -28,6 +90,13 @@ const RootQuery = new GraphQLObjectType({
             type: ProfileType,
             resolve(parent, args) {
                 return axios.get(`https://api.discogs.com/users/eckosneekz?token=${ process.env.DISCOGS_API_TOKEN }`)
+                    .then(res => res.data)
+            }
+        },
+        collection: {
+            type: CollectionType,
+            resolve(parent, args) {
+                return axios.get(`https://api.discogs.com/users/eckosneekz/collection/folders/0/releases?sort=added&sort_order=desc&per_page=50&token=${ process.env.DISCOGS_API_TOKEN }`)
                     .then(res => res.data)
             }
         }
