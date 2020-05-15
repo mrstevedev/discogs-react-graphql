6ff4ad796854b8a4fbc0c98331abba743a1145ee
Steven Polido <steven.polido@vistage.com>
2020-05-14 21:20:50 -0700

    Added to component to query and display GraphQL response data

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -1,13 +1,58 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import classnames from 'classnames';
+import Moment from 'react-moment';
 
-class Collection extends Component {
-    render() {
-        return (
-            <div>
-                Collection
-            </div>
-        )
+export const COLLECTION_QUERY = gql`
+    query CollectionQuery  {
+        collection {
+            releases {
+                date_added
+                basic_information {
+                    id
+                    year
+                    title
+                    artists {
+                        name
+                    }
+                    cover_image
+                }
+            }
+        }
     }
+`;
+
+function Collection()  {
+    const { loading, error, data } = useQuery(COLLECTION_QUERY);
+    if(loading) return <h4>Loading..</h4>;
+    if(error) console.log(error);
+    console.log(data);
+    const {
+        date_added,
+        basic_information,
+        artists
+    } = data.collection.releases
+    
+    return (
+        <div className="discogs__collection--responseContainer"
+        data-aos="fade-in" 
+        data-aos-delay="100"
+        data-aos-duration="10"
+        data-aos-easing="ease-in-out">
+            { data.collection.releases.map( release => (
+                <div className="discogs__collection--img" key={ release.basic_information.id }>
+                    <div className="discogs__collection--overlay">
+                        <div className="discogs__collection--overlay-inner"> 
+                            <p>{ release.basic_information.artists[0].name } - { release.basic_information.title }</p>
+                            <p>Added to collection on <Moment format="MMMM DD, YYYY">{ release.date_added }</Moment></p>
+                        </div>
+                    </div>
+                    <img src={ `${ release.basic_information.cover_image }` } />
+                </div>
+            )) }
+        </div>
+    )
 }
 
 export default Collection
