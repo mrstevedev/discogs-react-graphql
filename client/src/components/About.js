f49dd0b145c51a10d168e1fb359e601b63db9119
Steven Polido <steven.polido@vistage.com>
2020-05-14 17:50:51 -0700

    Added About.js markup

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 function About() {
     return (
-        <div>
-            About
+        <div data-aos="fade-in">
+            <p className="discogs__about--text">
+                Full stack application with Reactjs, with useEffect, useState hooks, CSS3, BEM, ES6 Javascript, Nodejs, Express, Webpack, Babel, & GraphQL using the Discogs API.
+            </p>
         </div>
     )
 }
