7deed431752cab088caa1fa8cf3e9e32d0e9494a
Steven Polido <steven.polido@vistage.com>
2020-05-19 19:18:40 -0700

    Added tech images

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import ReactImg from '../../public/img/React.png';
+import NodejsImg from '../../public/img/Nodejs.png';
+// import GraphQLImg from '../../public/img/GraphQLImg.svg';
 
 function About() {
     return (
         <div data-aos="fade-in">
             <p className="discogs__about--text">
                 Full stack application with Reactjs, with useEffect, useState hooks, CSS3, BEM, ES6 Javascript, Nodejs, Express, Webpack, Babel, & GraphQL using the Discogs API.
+            </p>
+            <p>
+                <img src={ ReactImg } />
+                <img src={ NodejsImg } />
             </p>
         </div>
     )
