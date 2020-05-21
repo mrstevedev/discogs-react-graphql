51e76f49008ba0fcd6de8b09e68b6b357bc3d7d2
Steven Polido <steven.polido@vistage.com>
2020-05-20 18:01:35 -0700

    Remove AOS

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,7 @@ import NodejsImg from '../../public/img/Nodejs.png';
 
 function About() {
     return (
-        <div data-aos="fade-in">
+        <div>
             <p className="discogs__about--text">
                 Full stack application with Reactjs, with useEffect, useState hooks, CSS3, BEM, ES6 Javascript, Nodejs, Express, Webpack, Babel, & GraphQL using the Discogs API.
             </p>
