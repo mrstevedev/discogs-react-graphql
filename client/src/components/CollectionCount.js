9568e16fd752ac48f60f5044a89969000c9c60fe
Steven Polido <steven.polido@vistage.com>
2020-05-21 21:00:37 -0700

    Added jsx markup and passed props to ColletionCount.js component

--- client/src/components/CollectionCount.js
+++ client/src/components/CollectionCount.js
@@ -1,10 +1,15 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-function CollectionCount() {
-    return (
-        <Fragment>
-            
-        </Fragment>
-    )
+function CollectionCount(props) {
+  return (
+    <Fragment>
+      <div className="discogs__profile--section">
+        <h1 className="discogs__profile--header">Collection</h1>
+        <span className="discogs__profile--sub">
+          {props.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </span>
+      </div>
+    </Fragment>
+  );
 }
 export default CollectionCount;
