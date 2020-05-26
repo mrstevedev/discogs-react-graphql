730965c5a646cde730168b70b4f0beff8c7baad4
Steven Polido <steven.polido@vistage.com>
2020-05-26 16:37:49 -0700

    Added Logo.js component

---
+++ client/src/components/Logo.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import Logo from '../../public/img/discogs-logo.svg';
+
+function Logo() {
+    return (
+        <Fragment>
+            <img src={ Logo } />
+        </Fragment>
+    )
+}
+export default Logo;
