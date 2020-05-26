76781171560b6857a5c32f45d59badde918567b1
Steven Polido <steven.polido@vistage.com>
2020-05-26 14:30:38 -0700

    Added style object for BG image

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from 'react'
 
 function Modal(props) {
+    const bgImg = {
+        backgroundImage: `url('${ props.data.profile.avatar_url }')`,
+        backgroundRepeat: 'no-repeat'
+    };
     return (
         <Fragment>
             <div className={`overlay ${props.modalActive ? 'show': ''}`}>
                     <div className="modal-close">
                         <a href="#" onClick={props.handleModalClose}><i className="fas fa-times"></i></a>
                     </div>
-                    <div className="modal">
-                        <img src="../../public/img/joehenderson.jpg" />
-
-                        <div>
+                    <div className="modal" style={bgImg}><div>
                             <a href="#" onClick={props.handleModalClose}>Close</a>
                         </div>
                     </div>
