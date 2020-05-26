97ccdca693eb011eaf511205b03dc2f84149e8db
Steven Polido <steven.polido@vistage.com>
2020-05-26 14:41:32 -0700

    Added AOS properties to avatar

--- client/src/components/ProfileAvatar.js
+++ client/src/components/ProfileAvatar.js
@@ -6,7 +6,11 @@ function ProfileAvatar(props) {
   };
   return (
     <Fragment>
-      <a href="#" onClick={props.handleModalOpen}>
+      <a href="#" onClick={props.handleModalOpen}    
+        data-aos="fade-in" 
+        data-aos-delay="100"
+        data-aos-duration="10"
+        data-aos-easing="ease-in-out">
         <div className="discogs__profile--img" style={bgImg}></div>
       </a>
     </Fragment>
